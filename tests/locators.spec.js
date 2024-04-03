7f2d8ac54d9291565558a21a81b2a13fdb677e12
Álvaro Ramírez Lorente <e_arimir@mercadona.com>
2024-04-03 14:06:34 +0200

    Remove only

--- tests/locators.spec.js
+++ tests/locators.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test.only('Playwright special locators', async ({ page }) => {
+test('Playwright special locators', async ({ page }) => {
     await page.goto("https://rahulshettyacademy.com/angularpractice/");
     await page.getByLabel("Check me out if you Love IceCreams!").click();
     await page.getByLabel("Employed").check();
