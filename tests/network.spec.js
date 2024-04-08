40859ef3cf901f9dd364fbe37048d6435616e500
Álvaro Ramírez Lorente <e_arimir@mercadona.com>
2024-04-08 18:24:47 +0200

    Intercept test

--- tests/network.spec.js
+++ tests/network.spec.js
@@ -1,4 +1,4 @@
-const { test, expect, request } = require('@playwright/test');
+const { test, request } = require('@playwright/test');
 const { APIUtils } = require('./utils/APIUtils');
 const loginPayload = { userEmail: "pmdzjbrgbhwnkescia@tpwlb.com", userPassword: "Iamking@000" }
 const orderPayload = { orders: [{ country: "Spain", productOrderedId: "6581ca399fd99c85e8ee7f45" }] }
