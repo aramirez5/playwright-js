1d0ff92d576c72cb1cadc575b4631adaab9c61eb
Álvaro Ramírez Lorente <e_arimir@mercadona.com>
2024-04-11 18:46:59 +0200

    Edit gitflow

--- playwright.flow.config.js
+++ playwright.flow.config.js
@@ -13,7 +13,7 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: false,
   /* Retry on CI only */
@@ -21,7 +21,7 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [[process.env.CI ? 'github' : 'html', { open: 'on-failure' }]],
+  reporter: [[process.env.CI ? 'github' : 'html', { open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
