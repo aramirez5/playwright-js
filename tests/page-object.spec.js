4877b4acc8fd889112b9a50ab2ddfa7d52847597
Álvaro Ramírez Lorente <e_arimir@mercadona.com>
2024-04-12 16:06:12 +0200

    Fix test

--- tests/page-object.spec.js
+++ tests/page-object.spec.js
@@ -63,7 +63,7 @@ for (const data of dataSet) {
     // CHECK ORDER ID IN SUMMARY IS THE SAME IN DETAILS VIEW
     await page.locator("//button[@routerlink='/dashboard/myorders']").click();
     await page.locator("tbody").waitFor();
-    const rows = await page.locator("tbody tr")
+    const rows = await page.locator("tbody tr");
     for (let i = 0; i < await rows.count(); i++) {
       const rowOrderId = await rows.nth(i).locator("th").textContent();
       if (orderId.includes(rowOrderId)) {
@@ -121,7 +121,7 @@ for (const data of dataSet) {
     // CHECK ORDER ID IN SUMMARY IS THE SAME IN DETAILS VIEW
     await page.locator("//button[@routerlink='/dashboard/myorders']").click();
     await page.locator("tbody").waitFor();
-    const rows = await page.locator("tbody tr")
+    const rows = await page.locator("tbody tr");
     for (let i = 0; i < await rows.count(); i++) {
       const rowOrderId = await rows.nth(i).locator("th").textContent();
       if (orderId.includes(rowOrderId)) {
